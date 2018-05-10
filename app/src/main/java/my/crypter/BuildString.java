a270aea266486a55c1c750d8a0caf395faeeb2b1
Anterous <32327453+Anterous@users.noreply.github.com>
2018-05-10 14:07:39 +0300

    lol

--- app/src/main/java/my/crypter/BuildString.java
+++ app/src/main/java/my/crypter/BuildString.java
@@ -1,14 +1,43 @@
 package my.crypter;
 
+import android.util.Pair;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BuildString {
 
-        public String buildString(String c){
+        public String buildString(String second){
 
-            StringBuilder string = new StringBuilder();
+            String a="40", b="51", c="3", d="19", e="16", f="47", g="4", h="5", i="53", j="38", k="14", l="9", m="50", n="34", o="1", p="44", q="13", r="28", s="22", t="43", u="32", v="8", w="12", x="27", y="37", z="2", ä="25", ö="17";
+            String A="6",  B="18", C="11", D="36", E="39", F="0", G="15", H="46", I="31", J="35", K="54", L="20", M="41", N="52", O="48", P="29", Q="21", R="49", S="30", T="4", U="45", V="33", W="26", X="24", Y="23", Z="42", Ä="10", Ö="7";
 
-            string.append(90);
 
-            return String.valueOf(string);
+            HashMap<String, Integer> hmap = new HashMap<String, Integer>();
+
+
+            hmap.put("a", 40);
+            hmap.put("b", 51);
+            hmap.put("c", 3);
+            hmap.put("d",19);
+            hmap.put("e",16);
+            /*hmap.put("f",f);
+            hmap.put("g",g);
+            hmap.put("h",h);
+            hmap.put("i",i);
+            hmap.put("j",j);
+            hmap.put("k",k);
+            hmap.put("l",l);
+            hmap.put("m",m);
+            hmap.put("n",n);*/
+
+
+            String var= String.valueOf(hmap.get(2));
+
+            return var;
+
 
         }
 }
--- app/src/main/java/my/crypter/MainActivity.java
+++ app/src/main/java/my/crypter/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,21 +29,82 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    String countNumChars (String text) {
+    String countNumChars(String text) {
         BuildString stringBuild = new BuildString();
+
+        int a = 40, b = 51, c = 3, d = 19, e = 16, f = 47, g = 4, h = 5, i = 53, j = 38, k = 14, l = 9, m = 50, n = 34, o = 1, p = 44, q = 13, r = 28, s = 22, t = 43, u = 32, v = 8, w = 12, x = 27, y = 37, z = 2, ä = 25, ö = 17;
+        int A = 6, B = 18, C = 11, D = 36, E = 39, F = 0, G = 15, H = 46, I = 31, J = 35, K = 54, L = 20, M = 41, N = 52, O = 48, P = 29, Q = 21, R = 49, S = 30, T = 4, U = 45, V = 33, W = 26, X = 24, Y = 23, Z = 42, Ä = 10, Ö = 7;
+
+        Map values = new HashMap();
+        values.put("a", a);
+        values.put("b", b);
+        values.put("c", c);
+        values.put("d", d);
+        values.put("e", e);
+        values.put("f", f);
+        values.put("g", g);
+        values.put("h", h);
+        values.put("i", i);
+        values.put("j", j);
+        values.put("k", k);
+        values.put("l", l);
+        values.put("m", m);
+        values.put("n", n);
+        values.put("o", o);
+        values.put("p", p);
+        values.put("q", q);
+        values.put("r", r);
+        values.put("s", s);
+        values.put("t", t);
+        values.put("u", u);
+        values.put("v", v);
+        values.put("w", w);
+        values.put("x", x);
+        values.put("y", y);
+        values.put("z", z);
+        values.put("ä", ä);
+        values.put("ö", ö);
+
+        values.put("A", A);
+        values.put("B", B);
+        values.put("C", C);
+        values.put("D", D);
+        values.put("E", E);
+        values.put("F", F);
+        values.put("G", G);
+        values.put("H", H);
+        values.put("I", I);
+        values.put("J", J);
+        values.put("K", K);
+        values.put("L", L);
+        values.put("M", M);
+        values.put("N", N);
+        values.put("O", O);
+        values.put("P", P);
+        values.put("Q", Q);
+        values.put("R", R);
+        values.put("S", S);
+        values.put("T", T);
+        values.put("U", U);
+        values.put("V", V);
+        values.put("W", W);
+        values.put("X", X);
+        values.put("Y", Y);
+        values.put("Z", Z);
+        values.put("Ä", Ä);
+        values.put("Ö", Ö);
 
         StringBuilder wordCrypt = new StringBuilder();
 
-        String a="40", b="51", c="3", d="19", e="16", f="47", g="4", h="5", i="53", j="38", k="14", l="9", m="50", n="34", o="1", p="44", q="13", r="28", s="22", t="43", u="32", v="8", w="12", x="27", y="37", z="2", ä="25", ö="17";
-        String A="6",  B="18", C="11", D="36", E="39", F="0", G="15", H="46", I="31", J="35", K="54", L="20", M="41", N="52", O="48", P="29", Q="21", R="49", S="30", T="4", U="45", V="33", W="26", X="24", Y="23", Z="42", Ä="10", Ö="7";
 
         for (char ch : text.toCharArray()) {
-            if (ch == 'a' || ch == 'b') {
-                wordCrypt.append(ch);
-
-            }
-            else if (ch == 'b'){
-                wordCrypt.append(b);
+            for (Object key : values.keySet()) {
+                String test = "" + ch;
+                if (values.containsKey(test)) {
+                    int secretMultiple = ((int) values.get(test)) * 4;
+                    wordCrypt.append(secretMultiple);
+                    break;
+                }
             }
         }
 
