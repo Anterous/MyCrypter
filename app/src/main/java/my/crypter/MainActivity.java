e421dc40923b11ad6152efd0cc862a1ce57fab06
Anterous <32327453+Anterous@users.noreply.github.com>
2018-05-10 12:19:15 +0300

    lol

--- app/src/main/java/my/crypter/MainActivity.java
+++ app/src/main/java/my/crypter/MainActivity.java
@@ -2,6 +2,9 @@ package my.crypter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cryptIt(View view) {
+        EditText wordToCrypt = findViewById(R.id.getWordToCrypt);
+        TextView cryptedWord = findViewById(R.id.cryptedWord);
+
+        String text = wordToCrypt.getText().toString();
+
+        String crypt = countNumChars(text);
+
+        cryptedWord.setText(crypt);
+
+    }
+
+    String countNumChars (String text) {
+        StringBuilder wordCrypt = new StringBuilder();
+
+        String a = "40",b = "30";
+
+        for (char c : text.toCharArray()) {
+            if (c == 'a') {
+                wordCrypt.append(a);
+
+            }
+            else if (c == 'b'){
+                wordCrypt.append(b);
+            }
+        }
+
+        return String.valueOf(wordCrypt);
     }
 }
---
+++ app/src/main/java/my/crypter/ValuePair.java
@@ -0,0 +1,37 @@
+package my.crypter;
+
+public class ValuePair {
+
+       /* private String x = 0.0;
+        private int y = 0.0;
+
+        public ValuePair(String x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public ValuePair()
+        {
+
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+
+    }*/
+}
