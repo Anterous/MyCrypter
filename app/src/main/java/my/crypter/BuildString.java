1924d6b73bc547a37403855053256eee64256d60
Anterous <32327453+Anterous@users.noreply.github.com>
2018-05-10 12:32:18 +0300

    lol

---
+++ app/src/main/java/my/crypter/BuildString.java
@@ -0,0 +1,14 @@
+package my.crypter;
+
+public class BuildString {
+
+        public String buildString(String c){
+
+            StringBuilder string = new StringBuilder();
+
+            string.append(c);
+
+            return String.valueOf(string);
+
+        }
+}
